caf0c34a36a7446d1b82d83870c53342c71f5ac6
Ciosepp <electro.syst8@gmail.com>
2025-01-29 15:50:14 +0100

    build ok

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,12 @@
 
 #include "nixieDisplay.h"
 
+#include "uRTCLib.h"
+
+// uRTCLib rtc;
+uRTCLib rtc(0x68);
+
+
 #define CLOCK_PIN 2
 #define DATA_PIN 3
 #define SAMPLE_PIN 4
@@ -12,22 +18,17 @@ nixieDisplay n(CLOCK_PIN,DATA_PIN,SAMPLE_PIN);
 #define REFRESH_TIME_uS 2000
 
 
-Timer T(MICROS);
-
-Timer Ttime(MILLIS);
-
-uint8_t digit=0;
-bool busy=false;
-
-
 
 void setup(){
 	n.nixieBegin(REFRESH_TIME_uS,REFRESH_DUTYCYCLE);
+	URTCLIB_WIRE.begin();
  
 }
 
 void loop(){
   n.displayNixie(37, (millis()/1000)%60, 0);
+  //n.displayNixie(rtc.hour(),rtc.minute(), 0);
+  
   
 }
 
